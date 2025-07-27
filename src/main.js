8c19f5dd4ac79f801042045b07ee24c46ab1a812
nicorobin28 <prayogoaskar@gmail.com>
2025-07-27 23:09:19 +0700

    allow all domain with cors

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
+import cors from "cors";
+
+web.use(cors());
 
 web.listen(300, () => {
   logger.info("App start");
